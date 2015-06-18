877c183fecca45d6c4ddbbd3726be82b812c79db
csinva <csinva@yahoo.com>
2015-06-18 11:31:35 -0400

    added scripts for running on cluster

---
+++ scripts/template.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export SPARK_HOME=${SPARK_HOME:-/usr/local/spark-current}
+export PATH=$SPARK_HOME:$SPARK_HOME/bin:$SPARK_HOME/sbin:$PATH
+
+JAR=`readlink -f "%s"`
+CLASS=%s
+
+if [ -z $PARALLELISM ]; then
+    echo "PARALLELISM not defined."
+    exit 1
+fi
+
+if [ -z $MASTER ]; then
+    echo "MASTER not defined."
+    exit 1
+fi
+
+$SPARK_HOME/bin/spark-submit --verbose \
+    --conf spark.default.parallelism=$PARALLELISM \
+    --class $CLASS \
+    $JAR "master="$MASTER $@
+    echo "CHANDAN"
+    echo $N_NODES
+#    $JAR "master=h01u25.int.janelia.org" $@
